8f2732ac9b5afabc2aa31c8c9a7d1cef3d3d1661
Christian Hamacher <christianh86@googlemail.com>
2018-01-10 19:53:02 +0100

    Removed Axios as dependancy in favor of fetch

--- src/api/wp.js
+++ src/api/wp.js
@@ -1,25 +1,26 @@
 /**
  * Request data from the Wordpress API.
  */
+import WP_API from '../../config/api';
 
-import axios from 'axios';
-import API from '../../config/app';
+// add trailing slash to url
+const BASE_URL = WP_API.baseUrl.replace(/\/?$/, '/');
 
 /**
  * Method for getting data from the provided end point url.
  *
- * @param {string} endPoint  Valid Wordpress API End point string.
+ * @param {string} endPoint  Valid Wordpress WP_API End point string.
  * @returns {Promise}
  */
 const api = (endPoint) => {
     return new Promise((resolve, reject) => {
-        // add trailing slash to url
-        let url = API.baseUrl.replace(/\/?$/, '/');
-        axios.get(url + endPoint).then((response) => {
-            resolve(response.data);
-        }).catch((error) => {
-            reject(error);
-        });
+        fetch(BASE_URL + endPoint)
+            .then((resp) => resp.json())
+            .then((response) => {
+                resolve(response);
+            }).catch((error) => {
+                reject(error);
+            });
     });
 };
 
@@ -28,8 +29,8 @@ const api = (endPoint) => {
  *
  * @returns {Promise}
  */
-function getPages() {
-    return api(API.endpoints.pages).then((response) => {
+function fetchPages() {
+    return api(WP_API.endpoints.pages).then((response) => {
         return response;
     });
 }
@@ -39,11 +40,14 @@ function getPages() {
  *
  * @returns {Promise}
  */
-function getPosts() {
-    return api(API.endpoints.posts).then((response) => {
+function fetchPosts() {
+    return api(WP_API.endpoints.posts).then((response) => {
         return response;
     });
 }
 
-export { getPages, getPosts }
+export {
+    fetchPages,
+    fetchPosts
+};
 
