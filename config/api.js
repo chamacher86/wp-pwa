89b8c2adb39a1277f16f4b9c30a03879ca3e296f
Christian Hamacher <christianh86@googlemail.com>
2018-01-10 19:50:43 +0100

    Renamed to api.js

---
+++ config/api.js
@@ -0,0 +1,9 @@
+const WP_API = {
+    baseUrl: 'https://demo.wp-api.org',
+    endpoints: {
+        pages: 'wp-json/wp/v2/pages',
+        posts: 'wp-json/wp/v2/posts'
+    }
+};
+
+export default WP_API;
